b8582faefa87f8242157beb4a575535e82726926
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2022-08-28 18:12:55 -0300

    parser: parse instructions for variable access

--- src/parser/instruction.rs
+++ src/parser/instruction.rs
@@ -4,6 +4,7 @@ use nom::{
     character::complete::{
         i32 as parse_i32, i64 as parse_i64, multispace0,
     },
+    combinator::value,
     error::context,
     number::complete::double as parse_f64,
     sequence::preceded,
@@ -14,10 +15,12 @@ use nom::{
 use super::{parse_index, parse_numerical_type, IResult};
 use crate::ast::{
     Index, Instruction, NumericalType, NumericalValue,
+    ScopeKind, VariableInstruction,
 };
 
 pub fn parse_instruction(input: &str) -> IResult<Instruction> {
     alt((
+        parse_variable_instruction,
         parse_const.map(|value| Instruction::Constant { value }),
         context("call", parse_call).map(Instruction::Call),
     ))(input)
@@ -29,13 +32,15 @@ pub fn parse_instruction(input: &str) -> IResult<Instruction> {
 /// Does not eat leading whitespace.
 ///
 /// ```
-/// use water::ast::NumericalValue;
+/// use water::ast::{NumericalValue, Instruction};
 /// use water::parser::parse_const;
+/// use water::parser::parse_instruction;
 ///
 /// assert_eq!(parse_const("i64.const -5"), Ok(("", NumericalValue::Int64(-5))));
-/// assert_eq!(parse_const("i32.const 5"), Ok(("", NumericalValue::Int32(5))));
+/// assert_eq!(parse_instruction("i32.const 5"), Ok(("", Instruction::Constant { value: NumericalValue::Int32(5) })));
 /// assert_eq!(parse_const("f64.const 5.5"), Ok(("", NumericalValue::Float64(5.5))));
 /// assert_eq!(parse_const("f32.const 2E-3"), Ok(("", NumericalValue::Float32(0.002))));
+/// assert_eq!(parse_instruction("f64.const 2e+5"), Ok(("", Instruction::Constant { value: NumericalValue::Float64(200000.0) })));
 /// ```
 pub fn parse_const(input: &str) -> IResult<NumericalValue> {
     // Parse the numerical type of this instruction: i32, i64,
@@ -95,4 +100,55 @@ pub fn parse_call(input: &str) -> IResult<Index> {
         multispace0,
         context("numerical index or identifier", parse_index),
     )(rest)
+}
+
+/// Parses an instruction for direct variable access.
+///
+/// Does not eat leading whitespace.
+///
+/// ```
+/// use water::ast::{ScopeKind, VariableInstruction, Instruction};
+/// use water::parser::parse_variable_instruction;
+///
+/// assert_eq!(
+///     parse_variable_instruction("local.set $idx 0"),
+///     Instruction::VariableInstruction {
+///         scope,
+///         instruction: opcode,
+///         index,
+///     }
+/// );
+/// ```
+pub fn parse_variable_instruction(
+    input: &str,
+) -> IResult<Instruction> {
+    let (rest, scope) = alt((
+        value(ScopeKind::Global, tag("global")),
+        value(ScopeKind::Local, tag("local")),
+    ))(input)?;
+
+    let parse_set = value(VariableInstruction::Set, tag(".set"));
+    let parse_get = value(VariableInstruction::Get, tag(".get"));
+    let parse_tee = value(VariableInstruction::Tee, tag(".tee"));
+
+    let (rest, opcode) = match scope {
+        ScopeKind::Global => {
+            // Ensure we don't parse `global.tee`
+            alt((parse_set, parse_get))(rest)?
+        }
+        ScopeKind::Local => {
+            alt((parse_set, parse_get, parse_tee))(rest)?
+        }
+    };
+
+    let (rest, index) =
+        preceded(multispace0, parse_index)(rest)?;
+
+    let instr = Instruction::VariableInstruction {
+        scope,
+        instruction: opcode,
+        index,
+    };
+
+    Ok((rest, instr))
 }
